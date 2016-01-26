92968ec56e73b42b4f412531da8f9e947366b24e
Christopher TruLove <c.t.trulove@gmail.com>
2016-01-26 15:57:01 -0700

    Moar...

--- partikles/partikles.js
+++ partikles/partikles.js
@@ -2,7 +2,12 @@
 // I figured it might be revealing to git this little monster step by step so the whole world can see my process.
 // ...having typed that now I believe that might be the worst idea ever.
 
-// 01/22/2015 - I built a little script that lets you embed an html generated "1up" the scrolls off the screen every
+/* Development Diary
+
+  01/22/2015 - I built a little script that lets you embed an html generated "1up" the scrolls off the screen every time you enter the Konami Code, in the process I revisited a bunch of ideas I had from the original particle tool I built for GEngHIS, I decided it was time to build those ideas out in a more modern more experienced way.
+  01/22/2015 - I started building a pseudo object, collecting everything I've done to date into some kind of framework I can actually manage
+
+*/
 
 function partikles( profileIn ){
 
@@ -27,23 +32,21 @@ particleProfile : {
 	border: true,						// simple border to create color 0 border, around the pixels in each particle
 
 	colorProfile:
-		[ '#000000', '#AAAAAA', '#FFFFFF'] ] // color table style, shapeProfile can refer to these colors as 0, 1, and 2
-		[ [1,1,1], [255,255,255] ] // random color range style, color generated will be within the range of colors in the set the example would produce any color
+		[ '#000', '#AAA', '#FFF'] ] 	// color table style, shapeProfile can refer to these colors as 0, 1, and 2
+		[ [1,1,1], [255,255,255] ] 		// random color range style, color generated will be within the range of colors in the set the example would produce any color
 
 	shapeProfile: [ [ 0, 1, 2 ], [1, 2, 2], [2, 1, 0] ],  // columns and rows referring to colors listed in the colorProfile variable, or, for monochromatic particles, true/false for on and off
 
-
-
-	hSpeed: 1,
-	ball: 0,
-	vSpeed: -1,
-	vSpeedVariance: 1,
-	color: ['#000000', '#FFFFFF', '#FFFFFF'], opacity: [20, 20, 0], shape: [ [ 0, 1, 0 ], [1, 1, 1], [1, 1, 1], [0, 1, 0] ]
+	direction: 180, 					// [dv]
+	velocity: 3,						// [dv]
 
 	//  Variants are +/- their base values, based on percentage.
 	//    eg. a base value of 20 with a variant of .5 can produce a result of 10-30, values more than 100% are considered 100%
 	decayVariance: 1,					// variation on length of decay, attached to particle at creation
 	birthVariance: .5,					// variation on births per cycle
+	velocityVariance: ,
+	directionVariance: ,
 
+	// Decay rules are simple array processes that allow values to change as the particle ages
 
 },
